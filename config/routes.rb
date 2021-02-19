cee34508fe3c7763151c5a79bc9d101c25ed8c29
daisuke.yamamura <gun_smoke_breakout@yahoo.co.jp>
2021-02-19 16:07:42 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'tasklists/create'
+  get 'tasklists/destroy'
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -10,4 +9,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+
+  resources :tasklist, only: [:create, :destroy]
 end
