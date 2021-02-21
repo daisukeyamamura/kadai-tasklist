778f30f1be58d8ba5c012c070d324c4683ae8467
daisuke.yamamura <gun_smoke_breakout@yahoo.co.jp>
2021-02-21 13:11:50 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasklists/create'
-  get 'tasklists/destroy'
-  root to: 'toppages#index'
-
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  root to: 'tasks#index'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
-
-  resources :tasklist, only: [:create, :destroy]
 end
